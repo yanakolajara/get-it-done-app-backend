fda00af8af48df8aee25ad9d979b0e81d320a60e
Yanako Lajara <127878396+yanakolajara@users.noreply.github.com>
2024-04-16 11:05:42 -0400

    database refactoring

--- db/schema.sql
+++ db/schema.sql
@@ -1,43 +1,43 @@
-DROP DATABASE IF EXISTS task_app;
-CREATE DATABASE task_app;
+DROP DATABASE IF EXISTS get_it_done_app;
+CREATE DATABASE get_it_done_app;
 
-\c task_app;
+\c get_it_done_app;
 
-DROP TABLE IF EXISTS childTasks;
-DROP TABLE IF EXISTS parentTasks;
+DROP TABLE IF EXISTS steps;
+DROP TABLE IF EXISTS tasks;
 DROP TABLE IF EXISTS users;
 
 --------* TASK SECTION ---------
 
 CREATE TABLE users(
     id SERIAL PRIMARY KEY,
-    firstName VARCHAR(20),
-    middleName VARCHAR(20) DEFAULT NULL,
-    lastName VARCHAR(50),
+    first_name VARCHAR(20),
+    middle_name VARCHAR(20) DEFAULT NULL,
+    last_name VARCHAR(50),
     email VARCHAR(100),
-    countryCode INT CHECK (countryCode >= 1 and countryCode <= 999),
+    country_code INT CHECK (country_code >= 1 and country_code <= 999),
     phone VARCHAR(10),
-    authToken VARCHAR(50)
+    auth VARCHAR(50)
 );
 
-CREATE TABLE parentTasks(
+CREATE TABLE tasks(
     id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES users(id),
     content TEXT,
-    progress_state INT CHECK (progress_state >= 1 and progress_state <= 3),
+    completed BOOLEAN DEFAULT FALSE,
     date VARCHAR(10),
     previews_task_id INTEGER,
     next_task_id INTEGER
 );
 
-CREATE TABLE childTasks(
+CREATE TABLE steps(
     id SERIAL PRIMARY KEY,
-    parentTask_id INTEGER REFERENCES parentTasks(id)
+    task_id INTEGER REFERENCES tasks(id)
     ON DELETE CASCADE,
     content TEXT,
     completed BOOLEAN DEFAULT FALSE,
     physical_energy INT CHECK (physical_energy >= 0 and physical_energy <= 4),
-    emotional_energy INT CHECK (physical_energy >= 0 and physical_energy <= 12),
-    previews_task_id INTEGER,
-    next_task_id INTEGER
+    emotional_energy INT CHECK (emotional_energy >= 0 and emotional_energy <= 12),
+    previews_step_id INTEGER,
+    next_step_id INTEGER
 );
--- db/seed.sql
+++ db/seed.sql
@@ -1,40 +1,40 @@
-\c task_app;
+\c get_it_done_app;
 
 -- Users
 INSERT INTO
-    users(firstName, middleName, lastName, email,countryCode,phone,authToken)
+    users(first_name, middle_name, last_name, email,country_code,phone,auth)
 VALUES
     ('Ynk', null, 'laj','ynk@test.com', 52, '1234567890', 'fgf98vYNY78OGFGT4%^UYT4TRTRG'),
     ('Luz', 'ele', 'piz','LUZ@test.com', 431, '9876543210', 'GFGT4%^UYT4TRTRGf98vYNY78Ogf');
 
 -- Parent tasks
 INSERT INTO
-    parentTasks(user_id, content, progress_state, date, previews_task_id, next_task_id)
+    tasks(user_id, content, completed, date, previews_task_id, next_task_id)
 VALUES
-    (1, 'Clean my desk', 2, '2024-04-06', 2, null),
-    (1, 'Organize closet', 1, '2024-04-06', null, 1);
-    -- (2, 'Study for exam', 1, '2024-03-10', 4, null),
-    -- (2, 'Exercise routine', 3, '2024-03-12', null, 3);
+    (1, 'Clean my desk', false, '2024-04-06', 2, null),
+    (1, 'Organize closet', false, '2024-04-06', null, 1),
+    (2, 'Study for exam', false, '2024-03-10', 4, null),
+    (2, 'Exercise routine', false, '2024-03-12', null, 3);
 
--- -- Child tasks
--- INSERT INTO
---     childTasks(parentTask_id, content, completed, physical_energy, emotional_energy , previews_task_id, next_task_id)
--- VALUES
---     -- Child tasks for Ynk's tasks
---     (1, 'Take the items out', false, 3, 2, null, 2),
---     (1, 'Clean the surface', false, 2, 1, 1, 3),
---     (1, 'Polish the wood', false, 3, 4, 2, 4),
---     (1, 'Put things back', false, 1, 2, 3, null),
---     (2, 'Take clothes out', false, 2, 2, null, 6),
---     (2, 'Sort clothes by type', false, 3, 3, 5, 7),
---     (2, 'Fold clothes neatly', false, 2, 3, 6, 8),
---     (2, 'Arrange clothes in closet', false, 2, null, 7, 9),
---     -- Child tasks for Luz's tasks
---     (3, 'Read Chapter 1', false, 2, 3,1, null),
---     (3, 'Complete Practice Problems', false, 3, 4, 2, 0),
---     (3, 'Review Lecture Notes', false, 2, 2, 3, 1),
---     (3, 'Take Quiz', false, 2, 2, 4, 2),
---     (4, 'Warm-up: Jogging', false, 3, 3, 1, null),
---     (4, 'Strength Training', false, 3, 4, 2, 0),
---     (4, 'Cardio: Running', false, 3, 3, 3, 1),
---     (4, 'Cool Down: Stretching', false, 2, 2, 4, 2);
+-- Child tasks
+INSERT INTO
+    steps(task_id, content, completed, physical_energy, emotional_energy , previews_step_id, next_step_id)
+VALUES
+    -- Child tasks for Ynk's tasks
+    (1, 'Take the items out', false, 3, 2, null, 2),
+    (1, 'Clean the surface', false, 2, 1, 1, 3),
+    (1, 'Polish the wood', false, 3, 4, 2, 4),
+    (1, 'Put things back', false, 1, 2, 3, null),
+    (2, 'Take clothes out', false, 2, 2, null, 6),
+    (2, 'Sort clothes by type', false, 3, 3, 5, 7),
+    (2, 'Fold clothes neatly', false, 2, 3, 6, 8),
+    (2, 'Arrange clothes in closet', false, 2, null, 7, 9),
+    -- Child tasks for Luz's tasks
+    (3, 'Read Chapter 1', false, 2, 3,1, null),
+    (3, 'Complete Practice Problems', false, 3, 4, 2, 0),
+    (3, 'Review Lecture Notes', false, 2, 2, 3, 1),
+    (3, 'Take Quiz', false, 2, 2, 4, 2),
+    (4, 'Warm-up: Jogging', false, 3, 3, 1, null),
+    (4, 'Strength Training', false, 3, 4, 2, 0),
+    (4, 'Cardio: Running', false, 3, 3, 3, 1),
+    (4, 'Cool Down: Stretching', false, 2, 2, 4, 2);
